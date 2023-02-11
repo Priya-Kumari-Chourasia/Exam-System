1d4ffda8f5276de9b215b7e8e9e080ec2f69d656
Priya Kumari <priya417789@gmail.com>
2023-02-11 14:47:00 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from "./Components/HomeComponent/Home";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import StudentLogin from "./Components/StudentComponent/StudentLogin/StudentLogin";
+import AdminLogin from "./Components/AdminComponent/AdminLogin";
+import AdminDashboard from "./Components/AdminComponent/AdminDashboard/AdminDashboard";
+import StudentDashboard from "./Components/StudentComponent/StudentDashboard/StudentDashboard";
 
-export default App;
+import {Route , BrowserRouter , Routes} from "react-router-dom";
+import StudentSignup from "./Components/StudentComponent/StudentSignup/StudentSignup";
+    function App(){
+      return (
+        <>
+          <BrowserRouter>
+              <Routes>
+                  <Route exact path="/" component={Home}></Route>
+                  <Route exact path="/StudentLogin" component={StudentLogin}></Route>
+                  <Route exact path="/StudentSignup" component={StudentSignup}></Route>
+                  <Route exact path="/AdminLogin" component={AdminLogin}></Route>
+                  <Route exact path="/AdminDashboard" component={AdminDashboard}></Route>
+                  <Route exact path="/StudentDashboard" component={StudentDashboard}></Route>
+              </Routes>
+          </BrowserRouter>
+        </>
+      );
+     
+    }
+
+      
+
+  export default App;
